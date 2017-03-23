a9d6b7d6189366f37226c9dccfcf3aa09b006718
lwinlwinoo <lwinlwinoo@LwinLwins-MacBook-Pro.local>
2017-03-23 11:39:45 +0800

    Guidelines and Calculator Core Data
    Table Creation for guidelines and calculator

---
+++ Trunk/Guidelines+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Guidelines+CoreDataClass.swift
+//  ICNutrition
+//
+//  Created by LwinLwin Oo on 23/3/17.
+//  Copyright © 2017 Cipher. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Guidelines)
+public class Guidelines: NSManagedObject {
+
+}
