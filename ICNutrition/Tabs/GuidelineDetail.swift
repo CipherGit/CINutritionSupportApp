be26cb999e47d3983779780d5daffaddc89c09e5
Justin Peña <jgvpena@gmail.com>
2017-04-20 03:29:10 +0800

    Modified Storyboard for Guideline details

---
+++ ICNutrition/Tabs/GuidelineDetail.swift
@@ -0,0 +1,35 @@
+//
+//  GuidelineDetail.swift
+//  ICNutrition
+//
+//  Created by Justin Peña on 20/4/17.
+//  Copyright © 2017 Cipher. All rights reserved.
+//
+
+import UIKit
+
+class GuidelineDetail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
